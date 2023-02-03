d219dc03c4f3e3870dc67559f8b3ae90eada7a41
hotelbooking1234 <123925661+hotelbooking1234@users.noreply.github.com>
2023-02-03 19:14:45 +0530

    Add files via upload

---
+++ RoomsManagementApp/src/main/java/com/roomsmanagement/app/RoomsManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.roomsmanagement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsManagementAppApplication.class, args);
+	}
+
+}
---
+++ RoomsManagementApp/src/test/java/com/roomsmanagement/app/RoomsManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomsmanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
